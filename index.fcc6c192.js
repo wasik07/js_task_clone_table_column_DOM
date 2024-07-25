1380d8edbdeccec0c2032cdd1f92da5a93e6b961
Vasyl Chukhrai <wasik1995@mail.com>
2024-07-25 20:40:59 +0300

    make build

---
+++ index.fcc6c192.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("tr").forEach(function(e){var l=e.firstElementChild.nextElementSibling.cloneNode(!0);e.lastElementChild.before(l)});
+//# sourceMappingURL=index.fcc6c192.js.map
